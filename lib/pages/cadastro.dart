bff5b85f62f71634745f4714638900516b1b2d58
Gustavo Monteiro <gustavomonteiro782@gmail.com>
2022-05-21 12:49:43 -0300

    Retirado paddings excessivos

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -25,108 +25,94 @@ class _CadastroPageState extends State<CadastroPage> {
         ),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(0.8),
+            padding: const EdgeInsets.all(16),
             child: SingleChildScrollView(
               reverse: true,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 20, 16, 20),
-                    child: Text(
-                      "INICIE O SEU CADASTRO",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'BankGothic md BT Medium',
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
+                  const Text(
+                    "INICIE O SEU CADASTRO",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'BankGothic md BT Medium',
+                      fontSize: 35,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
                   const SizedBox(height: 40),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Nome completo do usuário',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          // icon do TextField
-                          Icons.account_circle_rounded,
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Nome completo do usuário',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        // icon do TextField
+                        Icons.account_circle_rounded,
+                        color: Colors.black,
+                      ),
+                      // errorText 'xxx' informar erro dentro de uma validaçao  $variavel
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        // icon do TextField
+                        Icons.email_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 14),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        // icon do TextField
+                        Icons.lock_outline_rounded,
+                        color: Colors.black,
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 14),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Confirmar senha',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        // icon do TextField
+                        Icons.lock_outline_rounded,
+                        color: Colors.black,
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 40),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      minimumSize:
+                          Size(MediaQuery.of(context).size.width, 60),
+                    ),
+                    onPressed: () {},
+                    child: const SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'CADASTRAR',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
                           color: Colors.black,
-                        ),
-                        // errorText 'xxx' informar erro dentro de uma validaçao  $variavel
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          // icon do TextField
-                          Icons.email_outlined,
-                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18,
                         ),
                       ),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Senha',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          // icon do TextField
-                          Icons.lock_outline_rounded,
-                          color: Colors.black,
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Confirmar senha',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          // icon do TextField
-                          Icons.lock_outline_rounded,
-                          color: Colors.black,
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 90, 16, 10),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.amber,
-                        minimumSize:
-                            Size(MediaQuery.of(context).size.width, 60),
-                      ),
-                      onPressed: () {},
-                      child: const SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'CADASTRAR',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w800,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  )
                 ],
               ),
             ),
--- lib/pages/recuperar_senha.dart
+++ lib/pages/recuperar_senha.dart
@@ -23,71 +23,61 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
         ),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(0.8),
+            padding: const EdgeInsets.all(8),
             child: SingleChildScrollView(
               reverse: true,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 5, 16, 5),
-                    child: Text(
-                      "Esqueceu sua senha?",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'BankGothic md BT Medium',
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
+                  const Text(
+                    "Esqueceu sua senha?",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'BankGothic md BT Medium',
+                      fontSize: 35,
+                      fontWeight: FontWeight.w900,
                     ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 5, 16, 5),
-                    child: Text(
-                      "Por favor, informe seu E-mail associado a sua conta que enviaremos um link para o mesmo com as instruções para a restauração de sua senha.",
-                      style: TextStyle(
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Por favor, informe seu E-mail associado a sua conta que enviaremos um link para o mesmo com as instruções para a restauração de sua senha.",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'BankGothic md BT Medium',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        // icon do TextField
+                        Icons.email_outlined,
                         color: Colors.black,
-                        fontFamily: 'BankGothic md BT Medium',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
                       ),
                     ),
                   ),
                   const SizedBox(height: 20),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          // icon do TextField
-                          Icons.email_outlined,
-                          color: Colors.black,
-                        ),
-                      ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      minimumSize:
+                          Size(MediaQuery.of(context).size.width, 60),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 90, 16, 10),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.amber,
-                        minimumSize:
-                            Size(MediaQuery.of(context).size.width, 60),
-                      ),
-                      onPressed: () {},
-                      child: const SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'ENVIAR',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w800,
-                            fontSize: 18,
-                          ),
+                    onPressed: () {},
+                    child: const SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'ENVIAR',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 18,
                         ),
                       ),
                     ),
